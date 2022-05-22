89cb1fde51811f189e2c733f08d26d7d8f8b4572
chxm45 <106014930+chxm45@users.noreply.github.com>
2022-05-22 12:59:58 +0200

    Add files via upload

---
+++ exterieur.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="exterieur" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="../tileset/kisspng-tile-based-video-game-tiled-sprite-map-rpg-5ab7a449aca560.5776814215219845857072.png" width="1024" height="1024"/>
+ <wangsets>
+  <wangset name="chemin" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="16" wangid="1,1,0,0,0,1,1,1"/>
+   <wangtile tileid="17" wangid="1,1,1,1,0,0,0,1"/>
+   <wangtile tileid="48" wangid="0,0,0,1,1,1,1,1"/>
+   <wangtile tileid="49" wangid="0,1,1,1,1,1,0,0"/>
+   <wangtile tileid="79" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="80" wangid="0,0,0,1,1,1,0,0"/>
+   <wangtile tileid="81" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="111" wangid="0,1,1,1,0,0,0,0"/>
+   <wangtile tileid="112" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="113" wangid="0,0,0,0,0,1,1,1"/>
+   <wangtile tileid="143" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="144" wangid="1,1,0,0,0,0,0,1"/>
+   <wangtile tileid="145" wangid="0,0,0,0,0,0,0,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
---
+++ passages.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="passages" tilewidth="32" tileheight="32" tilecount="16" columns="8">
+ <image source="../tileset/passages.png" trans="ff00ff" width="256" height="64"/>
+</tileset>
